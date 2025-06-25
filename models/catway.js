bb1e5cae8ef442cc26822609fda65bb6e832636c
Alex <alexislouatiere@gmail.com>
2025-06-25 16:57:29 +0200

    creation des models pour les users, les catways ainsi que les reservations

---
+++ models/catway.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const catwaySchema = new mongoose.Schema({
+    catwayNumber :  {type: Number, unique: true},
+    catwayType :    {type: String, "short" || "long"},
+    catwayState :   {type: String}
+});
+
+module.exports = mongoose.model('Catway', catwaySchema);
---
+++ models/reservation.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    catwayNumber:   {type: mongoose.Schema.Types.ObjectId, ref: 'Catway', required: true},
+    clientName:     {type: String, required: true},
+    boatName:       {type: String},
+    startDate:      {type: Date, require: true},
+    endDate:        {type: Date, require: true}
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
